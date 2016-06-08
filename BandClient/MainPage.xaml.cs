da00aa6219d5edd4fe1e075bbd4dc16f68a84a74
sergiy baydachnyy <sergiy baydachnyy>
2016-06-07 21:10:01 -0700

    background task added

--- BandClient/MainPage.xaml.cs
+++ BandClient/MainPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Band;
+using Microsoft.Band.Notifications;
 using Microsoft.Band.Sensors;
+using Microsoft.Band.Tiles;
+using Microsoft.Band.Tiles.Pages;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,6 +12,9 @@ using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -16,6 +22,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -28,6 +35,10 @@ namespace BandClient
     public sealed partial class MainPage : Page
     {
         IBandClient bandClient;
+        bool isStarted = false;
+        Guid myTileId;
+        bool isMaxNotified=false;
+        bool isMinNotified = false;
 
         public MainPage()
         {
@@ -65,6 +76,22 @@ namespace BandClient
                     bandClient.SensorManager.HeartRate.ReadingChanged += HeartRate_ReadingChanged;
                     bool res = await bandClient.SensorManager.HeartRate.StartReadingsAsync();
                     VisualStateManager.GoToState(this, "Normal", false);
+
+                    myTileId = new Guid("5E67A3C2-39D1-4F9B-BBFF-0D81CCE6D317");
+                    BandTile myTile = new BandTile(myTileId)
+                    {
+                        Name = "My Tile",
+                        TileIcon = await LoadIcon("ms-appx:///Assets/SampleTileIconLarge.png"),
+                        SmallIcon = await LoadIcon("ms-appx:///Assets/SampleTileIconSmall.png")
+                    };
+
+                    IEnumerable<BandTile> tiles = await bandClient.TileManager.GetTilesAsync();
+                    //await bandClient.TileManager.RemoveTileAsync(myTileId);
+
+                    if ((await bandClient.TileManager.GetRemainingTileCapacityAsync() > 0)&&(tiles.Count()==0))
+                    {
+                        await bandClient.TileManager.AddTileAsync(myTile);
+                    }
                 }
 
             }
@@ -74,23 +101,81 @@ namespace BandClient
             }
         }
 
+        private async Task<BandIcon> LoadIcon(string uri)
+        {
+            StorageFile imageFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri(uri));
+
+            using (IRandomAccessStream fileStream = await imageFile.OpenAsync(FileAccessMode.Read))
+            {
+                WriteableBitmap bitmap = new WriteableBitmap(1, 1);
+                await bitmap.SetSourceAsync(fileStream);
+                return bitmap.ToBandIcon();
+            }
+        }
+
         protected async override void OnNavigatedFrom(NavigationEventArgs e)
         {
             if (bandClient != null)
             {
-                await bandClient.SensorManager.HeartRate.StartReadingsAsync();
+                await bandClient.SensorManager.HeartRate.StopReadingsAsync();
                 bandClient.Dispose();
+                if (isStarted)
+                {
+                    SaveValuesForBackground();
+                }
             }
             base.OnNavigatedFrom(e);
+        }
+
+        private void SaveValuesForBackground()
+        {
+            ApplicationData.Current.LocalSettings.Values["isStarted"] = isStarted;
+            ApplicationData.Current.LocalSettings.Values["myTileId"] = myTileId;
+            ApplicationData.Current.LocalSettings.Values["isMaxNotified"] = isMaxNotified;
+            ApplicationData.Current.LocalSettings.Values["isMinNotified"] = isMinNotified;
+            ApplicationData.Current.LocalSettings.Values["maxRate"] = heartData.MaxRate;
+            ApplicationData.Current.LocalSettings.Values["minRate"] = heartData.MinRate;
+        }
+
+        private void LoadValuesFromBackground()
+        {
+            if (ApplicationData.Current.LocalSettings.Values["isStarted"]!=null)
+            {
+                isStarted = (bool)ApplicationData.Current.LocalSettings.Values["isStarted"];
+                isMinNotified = (bool)ApplicationData.Current.LocalSettings.Values["isMinNotified"];
+                isMaxNotified = (bool)ApplicationData.Current.LocalSettings.Values["isMaxNotified"];
+                heartData.MaxRate = (int)ApplicationData.Current.LocalSettings.Values["maxRate"];
+                heartData.MinRate = (int)ApplicationData.Current.LocalSettings.Values["minRate"];
+            }
         }
 
         private async void HeartRate_ReadingChanged(object sender, Microsoft.Band.Sensors.BandSensorReadingEventArgs<Microsoft.Band.Sensors.IBandHeartRateReading> e)
         {
             await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-            () =>
+            async () =>
             {
                 if (e.SensorReading.Quality == HeartRateQuality.Locked)
+                {
                     heartData.CurrentRate = e.SensorReading.HeartRate;
+                    if (isStarted)
+                    {
+                        if ((heartData.CurrentRate>=heartData.MaxRate)&&(!isMaxNotified))
+                        {
+                            isMaxNotified = true;
+                            isMinNotified = false;
+                            await bandClient.NotificationManager.SendMessageAsync(myTileId, "Workload Demo", "You have to rest some time!", DateTimeOffset.Now, MessageFlags.ShowDialog);
+                            await bandClient.NotificationManager.VibrateAsync(VibrationType.ThreeToneHigh);
+
+                        }
+                        else if ((heartData.CurrentRate <= heartData.MinRate)&&(!isMinNotified)&&(isMaxNotified))
+                        {
+                            isMaxNotified = false;
+                            isMinNotified = true;
+                            await bandClient.NotificationManager.SendMessageAsync(myTileId, "Workload Demo", "You can continue", DateTimeOffset.Now, MessageFlags.ShowDialog);
+                            await bandClient.NotificationManager.VibrateAsync(VibrationType.TwoToneHigh);
+                        }
+                    }
+                }
              });
         }
 
@@ -114,13 +199,20 @@ namespace BandClient
                     VisualStateManager.GoToState(this, "NormalStarted", false);
                     heartData.MinRate = heartData.Rates[idx1];
                     heartData.MaxRate = heartData.Rates[idx2];
+                    isStarted = true;
+                    isMinNotified = false;
+                    isMaxNotified = false;
+                    await bandClient.NotificationManager.SendMessageAsync(myTileId, "Workload Demo", "The workload is started", DateTimeOffset.Now, MessageFlags.ShowDialog);
                 }
+                
             }
         }
 
-        private void stopButton_Click(object sender, RoutedEventArgs e)
+        private async void stopButton_Click(object sender, RoutedEventArgs e)
         {
             VisualStateManager.GoToState(this, "Normal", false);
+            await bandClient.NotificationManager.SendMessageAsync(myTileId, "Workload Demo", "Your workloaded is stopped", DateTimeOffset.Now, MessageFlags.ShowDialog);
+            isStarted = false;
         }
     }
 }
